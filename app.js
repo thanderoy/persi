2b369694c9f1a325707855c9d093de83790fe677
Roy Thande <roy.thande@gmail.com>
2023-07-16 12:38:56 +0300

    Approach II: Style the element

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 // Isolate the video player
-function isolateTarget() {
-    let x = document.getElementsByTagName("iframe");
-    console.log("found frame");
-    for (let t of x) {
-        if (t.id === "iframe-embed") return e;
-    }
-    return null;
-};
+// function isolateTarget() {
+//     let x = document.getElementsByTagName("iframe");
+//     console.log("found frame");
+//     for (let t of x) {
+//         if (t.id === "iframe-embed") return e;
+//     }
+//     return null;
+// };
 
 // Toggle Fullscreen
 function toogleFullscreen() {
     if (!document.fullscreenElement) {
-        let target = isolateTarget();
+        // let target = isolateTarget();
+        let target = document.querySelector('.player-frame');
 
-        if (target.requestFullscreen) {
-            target.requestFullscreen();
-        } else if (target.mozRequestFullScreen) { // Firefox
-            target.mozRequestFullScreen();
-        } else if (target.webkitRequestFullscreen) { // Chrome, Safari and Opera
-            target.webkitRequestFullscreen();
-        } else if (target.msRequestFullscreen) { // IE/Edge
-            target.msRequestFullscreen();
-        }
-
+        // Approach II
+        target.style.height = "100vh";
+        target.style.width = "100vw";
+        target.style.top = "0";
+        target.style.left = "0";
+        target.style.position = "fixed";
+        target.style.zIndex = "9999"
         console.log("[Persi] Video playback automatically scaled to fullscreen.")
     }
 };
