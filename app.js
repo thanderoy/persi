e02343de0e1734efc7ce9b97445582a2a3459d99
Thande Roy <39765073+thanderoy@users.noreply.github.com>
2023-08-13 23:35:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 // Isolate the video player
-// function isolateTarget() {
-//     let x = document.getElementsByTagName("iframe");
-//     console.log("found frame");
-//     for (let t of x) {
-//         if (t.id === "iframe-embed") return e;
-//     }
-//     return null;
-// };
-
-// Toggle Fullscreen
-function toogleFullscreen() {
-    if (!document.fullscreenElement) {
-        // let target = isolateTarget();
-        let target = document.querySelector('.player-frame');
-
-        // Approach II
-        target.style.height = "100vh";
-        target.style.width = "100vw";
-        target.style.top = "0";
-        target.style.left = "0";
-        target.style.position = "fixed";
-        target.style.zIndex = "9999"
-        console.log("[Persi] Video playback automatically scaled to fullscreen.")
+function isolateTarget() {
+    let x = document.getElementsByTagName("iframe");
+    for (let t of x) {
+        if (t.id === "iframe-embed") {
+            console.log("[Persi] -> Found player");
+            return t;
+        }
     }
+    console.log("[Persi] -> Failed to isolate player");
+    return null;
 };
 
-
+// Request fullscreenAPI 
+function requestFullScreen(element) {
+    if (!document.fullscreenElement) {
+        // Supports most browsers and their versions.
+        var requestMethod = element.requestFullScreen || 
+            element.webkitRequestFullScreen           || 
+            element.mozRequestFullScreen              || 
+            element.msRequestFullScreen;
+        if (requestMethod) {
+            requestMethod.call(element);
+        }
+    }
+}
 
 // Persistent
 window.onload = function() {
-    console.log("[Persi] Persi is running...");
-    toogleFullscreen();
-};
-
-
+    console.log('[Persi] is running...');
+    element = isolateTarget();
+    
+    element.onload = function() {
+        requestFullScreen(element);
+    }; 
+}
